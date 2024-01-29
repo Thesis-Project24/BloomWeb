48824e9396f35fa030aaf055bc615339d5afe2d7
khadhKadh <azizkh103@gmail.com>
2024-01-29 12:08:00 +0100

    articles done switching to doctors

--- BloomWeb/src/App.jsx
+++ BloomWeb/src/App.jsx
@@ -14,6 +14,7 @@ import {QueryClient,QueryClientProvider} from "@tanstack/react-query"
 import ArticleDet from "./assets/Components/Articles/ArticleDet";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import SavedArticles from "./assets/Components/Articles/SavedArticles";
 
 
 
@@ -29,6 +30,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/articles" element={<Articles />} />
           <Route path="/article/:id" element={<ArticleDet/>}/>
+          <Route path="/savedArticles" element={<SavedArticles/>}/>
           <Route path="/doctors" element={<Doctors />} />
           <Route path="/forum" element={<Forum />} />
           <Route path="/signin" element={<Signin />}/>
--- BloomWeb/src/assets/Components/Doctors/Doctors.jsx
+++ BloomWeb/src/assets/Components/Doctors/Doctors.jsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import React from "react";
+import { useFetchDoctors } from "../../Api/Doctors";
 
 const Doctors = () => {
+  const { data, isLoading, isError, isSuccess, refetch } = useFetchDoctors();
+  console.log(data);
   return (
     <div>
-      All Doctors here
+    <div className="flex justify-center mt-[50px] ">
+      <div className="p-[20px] border rounded-2xl">
+        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0 ">
+          <li className="hover:underline me-4 md:me-6 shadow-sm text-sm font-medium text-gray-50 sm:mb-0 cursor-pointer">
+            All Doctors
+          </li>
+          <li className="hover:underline me-4 md:me-6 shadow-sm cursor-pointer">
+            Psychiatry
+          </li>
+          <li className="hover:underline me-4 md:me-6 shadow-sm cursor-pointer">
+            psychology
+          </li>
+          <li className="hover:underline me-4 md:me-6 shadow-sm cursor-pointer">
+            Psychoneurosis
+          </li>
+          <li className="hover:underline me-4 md:me-6 shadow-sm cursor-pointer">
+            Neuropsychiatry
+          </li>
+          <li className="hover: me-4 md:me-6 shadow-sm cursor-pointer">
+            {">"}
+          </li>
+        </ul>
+      </div>
     </div>
-  )
-}
+      <div>
+        {}
+       </div>
+    </div>
+  );
+};
 
-export default Doctors
+export default Doctors;
